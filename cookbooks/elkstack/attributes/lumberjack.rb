e3ebbc6583640485abbf7c0c01bcfb147de58bfd
nevermore <lelun.chen@maichuang.net>
2016-12-09 10:46:40 +0800

    remove error elk cookbook

--- cookbooks/elkstack/attributes/lumberjack.rb
+++
@@ -1,6 +0,0 @@
-
-default['lumberjack']['ssl certificate'] = 'lumberjack.crt'
-default['lumberjack']['ssl key'] = 'lumberjack.key'
-default['lumberjack']['ssl ca'] = 'lumberjack.crt'
-default['lumberjack']['user'] = 'logstash'
-default['lumberjack']['group'] = 'logstash'
